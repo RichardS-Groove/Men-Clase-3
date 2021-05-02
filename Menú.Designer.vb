06819ab2d2fd7e01c4c4bb687aea99b40ee682f5
RichardS <richardsjcb@gmail.com>
2021-05-02 20:49:51 -0300

    Up

---
+++ Menú.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Menú
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bSalir = New System.Windows.Forms.Button()
+        Me.bClientes = New System.Windows.Forms.Button()
+        Me.bProveedores = New System.Windows.Forms.Button()
+        Me.bListados = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SistemaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListadosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BackupToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FacturasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IIBBToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComprasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProveedoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProductosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EntregasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'bSalir
+        '
+        Me.bSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.bSalir.Location = New System.Drawing.Point(77, 339)
+        Me.bSalir.Name = "bSalir"
+        Me.bSalir.Size = New System.Drawing.Size(75, 23)
+        Me.bSalir.TabIndex = 0
+        Me.bSalir.Text = "Salir"
+        Me.bSalir.UseVisualStyleBackColor = False
+        '
+        'bClientes
+        '
+        Me.bClientes.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.bClientes.Location = New System.Drawing.Point(315, 56)
+        Me.bClientes.Name = "bClientes"
+        Me.bClientes.Size = New System.Drawing.Size(75, 23)
+        Me.bClientes.TabIndex = 1
+        Me.bClientes.Text = "Clientes"
+        Me.bClientes.UseVisualStyleBackColor = False
+        '
+        'bProveedores
+        '
+        Me.bProveedores.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.bProveedores.Location = New System.Drawing.Point(314, 112)
+        Me.bProveedores.Name = "bProveedores"
+        Me.bProveedores.Size = New System.Drawing.Size(75, 23)
+        Me.bProveedores.TabIndex = 2
+        Me.bProveedores.Text = "Proveedores"
+        Me.bProveedores.UseVisualStyleBackColor = False
+        '
+        'bListados
+        '
+        Me.bListados.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bListados.Location = New System.Drawing.Point(315, 172)
+        Me.bListados.Name = "bListados"
+        Me.bListados.Size = New System.Drawing.Size(75, 23)
+        Me.bListados.TabIndex = 3
+        Me.bListados.Text = "Listados"
+        Me.bListados.UseVisualStyleBackColor = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SistemaToolStripMenuItem, Me.VentasToolStripMenuItem, Me.ComprasToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SistemaToolStripMenuItem
+        '
+        Me.SistemaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ListadosToolStripMenuItem, Me.BackupToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.SistemaToolStripMenuItem.Name = "SistemaToolStripMenuItem"
+        Me.SistemaToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.SistemaToolStripMenuItem.Text = "Sistema"
+        '
+        'ListadosToolStripMenuItem
+        '
+        Me.ListadosToolStripMenuItem.Name = "ListadosToolStripMenuItem"
+        Me.ListadosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ListadosToolStripMenuItem.Text = "Listados"
+        '
+        'BackupToolStripMenuItem
+        '
+        Me.BackupToolStripMenuItem.Name = "BackupToolStripMenuItem"
+        Me.BackupToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.BackupToolStripMenuItem.Text = "Backups"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'VentasToolStripMenuItem
+        '
+        Me.VentasToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClientesToolStripMenuItem, Me.FacturasToolStripMenuItem, Me.IIBBToolStripMenuItem})
+        Me.VentasToolStripMenuItem.Name = "VentasToolStripMenuItem"
+        Me.VentasToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.VentasToolStripMenuItem.Text = "Ventas"
+        '
+        'ClientesToolStripMenuItem
+        '
+        Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClientesToolStripMenuItem.Text = "Clientes"
+        '
+        'FacturasToolStripMenuItem
+        '
+        Me.FacturasToolStripMenuItem.Name = "FacturasToolStripMenuItem"
+        Me.FacturasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FacturasToolStripMenuItem.Text = "Facturas"
+        '
+        'IIBBToolStripMenuItem
+        '
+        Me.IIBBToolStripMenuItem.Name = "IIBBToolStripMenuItem"
+        Me.IIBBToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.IIBBToolStripMenuItem.Text = "IIBB"
+        '
+        'ComprasToolStripMenuItem
+        '
+        Me.ComprasToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProveedoresToolStripMenuItem, Me.ProductosToolStripMenuItem, Me.EntregasToolStripMenuItem})
+        Me.ComprasToolStripMenuItem.Name = "ComprasToolStripMenuItem"
+        Me.ComprasToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.ComprasToolStripMenuItem.Text = "Compras"
+        '
+        'ProveedoresToolStripMenuItem
+        '
+        Me.ProveedoresToolStripMenuItem.Name = "ProveedoresToolStripMenuItem"
+        Me.ProveedoresToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProveedoresToolStripMenuItem.Text = "Proveedores"
+        '
+        'ProductosToolStripMenuItem
+        '
+        Me.ProductosToolStripMenuItem.Name = "ProductosToolStripMenuItem"
+        Me.ProductosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProductosToolStripMenuItem.Text = "Productos"
+        '
+        'EntregasToolStripMenuItem
+        '
+        Me.EntregasToolStripMenuItem.Name = "EntregasToolStripMenuItem"
+        Me.EntregasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EntregasToolStripMenuItem.Text = "Entregas"
+        '
+        'Menú
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.bListados)
+        Me.Controls.Add(Me.bProveedores)
+        Me.Controls.Add(Me.bClientes)
+        Me.Controls.Add(Me.bSalir)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Menú"
+        Me.Text = "Menú"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents bSalir As Button
+    Friend WithEvents bClientes As Button
+    Friend WithEvents bProveedores As Button
+    Friend WithEvents bListados As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SistemaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListadosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BackupToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FacturasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IIBBToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ComprasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProveedoresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProductosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EntregasToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Menú.vb
@@ -0,0 +1,34 @@
+﻿Public Class Menú
+    Private Sub bSalir_Click(sender As Object, e As EventArgs) Handles bSalir.Click
+        If MessageBox.Show("Está seguro que desea salir?", "Salir del Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) = DialogResult.Yes Then End
+    End Sub
+
+    Private Sub bClientes_Click(sender As Object, e As EventArgs) Handles bClientes.Click
+        Dim cli As New Clientes
+        cli.BackColor = Color.Red
+        cli.ShowDialog()
+    End Sub
+
+    Private Sub bProveedores_Click(sender As Object, e As EventArgs) Handles bProveedores.Click
+        Dim prv As New Proveedores
+        prv.BackColor = Color.Green
+        prv.ShowDialog()
+    End Sub
+
+    Private Sub bListados_Click(sender As Object, e As EventArgs) Handles bListados.Click
+        Dim lst As New Listados
+        lst.BackColor = Color.Blue
+        lst.ShowDialog()
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        If MessageBox.Show("Está seguro que desea Salir?") Then End
+
+    End Sub
+
+    Private Sub ClientesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClientesToolStripMenuItem.Click
+        Dim cli As New Clientes
+        cli.BackColor = Color.Red
+        cli.ShowDialog()
+    End Sub
+End Class
